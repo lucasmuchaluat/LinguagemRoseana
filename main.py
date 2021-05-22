9c436a95508896a915c69657032cece3e061dbc3
Lucas Muchaluat <lucasmuchaluat@gmail.com>
2021-05-21 23:56:45 -0300

    entrega 2 feita

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ with open("./code.txt", "r") as f:
 lexer = Lexer().get_lexer()
 tokens = lexer.lex(text_input)
 
+for token in tokens:
+    print(token)
+
 pg = Parser()
 pg.parse()
 parser = pg.get_parser()
